8f193cf68c8affcfadb09d031e812c66bc41dd86
Wilfried Roset <wilfriedroset@users.noreply.github.com>
2022-11-23 14:15:11 +0100

    Align envvar name to both respect convention and by compatible with mimirtool
    Signed-off-by: Wilfried Roset <wilfriedroset@users.noreply.github.com>

--- mimirtool/provider.go
+++ mimirtool/provider.go
@@ -36,80 +36,83 @@ func New(version string, mimirClient mimirClientInterface) func() *schema.Provid
 	return func() *schema.Provider {
 		p := &schema.Provider{
 			Schema: map[string]*schema.Schema{
-				"url": {
+				// In order to allow users to use both terraform and mimirtool cli let's use the same envvar names
+				// We shall accept two envvar name: one to respect terraform convention <provider>_<resource_name> and the other one from mimirtool.
+				// terraform convention will be taken into account first.
+				"address": {
 					Type:         schema.TypeString,
 					Required:     true,
-					DefaultFunc:  schema.EnvDefaultFunc("MIMIR_ADDRESS", nil),
-					Description:  "Address to use when contacting Grafana Mimir. May alternatively be set via the `MIMIR_ADDRESS` environment variable.",
+					DefaultFunc:  schema.MultiEnvDefaultFunc([]string{"MIMIRTOOL_ADDRESS", "MIMIR_ADDRESS"}, nil),
+					Description:  "Address to use when contacting Grafana Mimir. May alternatively be set via the `MIMIRTOOL_ADDRESS` or `MIMIR_ADDRESS` environment variable.",
 					ValidateFunc: validation.IsURLWithHTTPorHTTPS,
 				},
 				"tenant_id": {
 					Type:        schema.TypeString,
 					Optional:    true,
-					DefaultFunc: schema.EnvDefaultFunc("MIMIR_TENANT_ID", nil),
-					Description: "Tenant ID to use when contacting Grafana Mimir. May alternatively be set via the `MIMIR_TENANT_ID` environment variable.",
+					DefaultFunc: schema.MultiEnvDefaultFunc([]string{"MIMIRTOOL_TENANT_ID", "MIMIR_TENANT_ID"}, nil),
+					Description: "Tenant ID to use when contacting Grafana Mimir. May alternatively be set via the `MIMIRTOOL_TENANT_ID` or `MIMIR_TENANT_ID` environment variable.",
 				},
-				"user": {
+				"api_user": {
 					Type:        schema.TypeString,
 					Optional:    true,
-					DefaultFunc: schema.EnvDefaultFunc("MIMIR_API_USER.", nil),
-					Description: "API user to use when contacting Grafana Mimir. May alternatively be set via the `MIMIR_API_USER` environment variable.",
+					DefaultFunc: schema.MultiEnvDefaultFunc([]string{"MIMIRTOOL_API_USER", "MIMIR_API_USER"}, nil),
+					Description: "API user to use when contacting Grafana Mimir. May alternatively be set via the `MIMIRTOOL_API_USER` or `MIMIR_API_USER` environment variable.",
 				},
-				"key": {
-					Type:        schema.TypeString,
-					Optional:    true,
-					Sensitive:   true,
-					DefaultFunc: schema.EnvDefaultFunc("MIMIR_API_KEY", ""),
-					Description: "API key to use when contacting Grafana Mimir. May alternatively be set via the `MIMIR_API_KEY` environment variable.",
-				},
-				"token": {
+				"api_key": {
 					Type:        schema.TypeString,
 					Optional:    true,
 					Sensitive:   true,
-					DefaultFunc: schema.EnvDefaultFunc("MIMIR_AUTH_TOKEN.", nil),
-					Description: "Authentication token for bearer token or JWT auth when contacting Grafana Mimir. May alternatively be set via the `MIMIR_AUTH_TOKEN` environment variable.",
+					DefaultFunc: schema.MultiEnvDefaultFunc([]string{"MIMIRTOOL_API_KEY", "MIMIR_API_KEY"}, nil),
+					Description: "API key to use when contacting Grafana Mimir. May alternatively be set via the `MIMIRTOOL_API_KEY` or `MIMIR_API_KEY` environment variable.",
+				},
+				"auth_token": {
+					Type:        schema.TypeString,
+					Optional:    true,
+					Sensitive:   true,
+					DefaultFunc: schema.MultiEnvDefaultFunc([]string{"MIMIRTOOL_AUTH_TOKEN", "MIMIR_AUTH_TOKEN"}, nil),
+					Description: "Authentication token for bearer token or JWT auth when contacting Grafana Mimir. May alternatively be set via the `MIMIRTOOL_AUTH_TOKEN` or `MIMIR_AUTH_TOKEN` environment variable.",
 				},
 				"tls_key_path": {
 					Type:        schema.TypeString,
 					Optional:    true,
-					DefaultFunc: schema.EnvDefaultFunc("MIMIR_TLS_KEY_PATH", nil),
-					Description: "Client TLS key file to use to authenticate to the MIMIR server. May alternatively be set via the `MIMIR_TLS_KEY_PATH` environment variable.",
+					DefaultFunc: schema.MultiEnvDefaultFunc([]string{"MIMIRTOOL_TLS_KEY_PATH", "MIMIR_TLS_KEY_PATH"}, nil),
+					Description: "Client TLS key file to use to authenticate to the MIMIR server. May alternatively be set via the `MIMIRTOOL_TLS_KEY_PATH` or `MIMIR_TLS_KEY_PATH` environment variable.",
 				},
 				"tls_cert_path": {
 					Type:        schema.TypeString,
 					Optional:    true,
-					DefaultFunc: schema.EnvDefaultFunc("MIMIR_TLS_CERT_PATH", nil),
-					Description: "Client TLS certificate file to use to authenticate to the MIMIR server. May alternatively be set via the `MIMIR_TLS_CERT_PATH` environment variable.",
+					DefaultFunc: schema.MultiEnvDefaultFunc([]string{"MIMIRTOOL_TLS_CERT_PATH", "MIMIR_TLS_CERT_PATH"}, nil),
+					Description: "Client TLS certificate file to use to authenticate to the MIMIR server. May alternatively be set via the `MIMIRTOOL_TLS_CERT_PATH` or `MIMIR_TLS_CERT_PATH` environment variable.",
 				},
-				"ca_cert_path": {
+				"tls_ca_path": {
 					Type:        schema.TypeString,
 					Optional:    true,
-					DefaultFunc: schema.EnvDefaultFunc("MIMIR_CA_CERT_PATH", nil),
-					Description: "Certificate CA bundle to use to verify the MIMIR server's certificate. May alternatively be set via the `MIMIR_CA_CERT_PATH` environment variable.",
+					DefaultFunc: schema.MultiEnvDefaultFunc([]string{"MIMIRTOOL_TLS_CA_PATH", "MIMIR_TLS_CA_PATH"}, nil),
+					Description: "Certificate CA bundle to use to verify the MIMIR server's certificate. May alternatively be set via the `MIMIRTOOL_TLS_CA_PATH` or `MIMIR_TLS_CA_PATH` environment variable.",
 				},
 				"insecure_skip_verify": {
 					Type:        schema.TypeBool,
 					Optional:    true,
-					DefaultFunc: schema.EnvDefaultFunc("MIMIR_INSECURE_SKIP_VERIFY", nil),
-					Description: "Skip TLS certificate verification. May alternatively be set via the `MIMIR_INSECURE_SKIP_VERIFY` environment variable.",
+					DefaultFunc: schema.MultiEnvDefaultFunc([]string{"MIMIRTOOL_INSECURE_SKIP_VERIFY", "MIMIR_INSECURE_SKIP_VERIFY"}, nil),
+					Description: "Skip TLS certificate verification. May alternatively be set via the `MIMIRTOOL_INSECURE_SKIP_VERIFY` or `MIMIR_INSECURE_SKIP_VERIFY` environment variable.",
 				},
 				"prometheus_http_prefix": {
 					Type:        schema.TypeString,
 					Optional:    true,
-					DefaultFunc: schema.EnvDefaultFunc("MIMIR_API_PREFIX", "/prometheus"),
-					Description: "Path prefix to use for rules. May alternatively be set via the `MIMIR_API_PREFIX` environment variable.",
+					DefaultFunc: schema.MultiEnvDefaultFunc([]string{"MIMIRTOOL_PROMETHEUS_HTTP_PREFIX", "MIMIR_PROMETHEUS_HTTP_PREFIX"}, "/prometheus"),
+					Description: "Path prefix to use for rules. May alternatively be set via the `MIMIRTOOL_PROMETHEUS_HTTP_PREFIX` or `MIMIR_PROMETHEUS_HTTP_PREFIX` environment variable.",
 				},
 				"alertmanager_http_prefix": {
 					Type:        schema.TypeString,
 					Optional:    true,
-					DefaultFunc: schema.EnvDefaultFunc("MIMIR_ALERTMANAGER_HTTP_PREFIX", "/alertmanager"),
-					Description: "Path prefix to use for alertmanager. May alternatively be set via the `MIMIR_ALERTMANAGER_HTTP_PREFIX` environment variable.",
+					DefaultFunc: schema.MultiEnvDefaultFunc([]string{"MIMIRTOOL_ALERTMANAGER_HTTP_PREFIX", "MIMIR_ALERTMANAGER_HTTP_PREFIX"}, "/alertmanager"),
+					Description: "Path prefix to use for alertmanager. May alternatively be set via the `MIMIRTOOL_ALERTMANAGER_HTTP_PREFIX` or `MIMIR_ALERTMANAGER_HTTP_PREFIX` environment variable.",
 				},
 				"store_rules_sha256": {
 					Type:        schema.TypeBool,
 					Optional:    true,
-					DefaultFunc: schema.EnvDefaultFunc("MIMIR_STORE_RULES_SHA256", false),
-					Description: "Set to true if you want to save only the sha256sum instead of namespace's groups rules definition in the tfstate.",
+					DefaultFunc: schema.MultiEnvDefaultFunc([]string{"MIMIRTOOL_STORE_RULES_SHA256", "MIMIR_STORE_RULES_SHA256"}, false),
+					Description: "Set to true if you want to save only the sha256sum instead of namespace's groups rules definition in the tfstate. May alternatively be set via the `MIMIRTOOL_STORE_RULES_SHA256` or `MIMIR_STORE_RULES_SHA256` environment variable.",
 				},
 			},
 			DataSourcesMap: map[string]*schema.Resource{},
@@ -149,14 +152,15 @@ func configure(version string, p *schema.Provider, mimirClient mimirClientInterf
 }
 
 func getDefaultMimirClient(d *schema.ResourceData) (mimirClientInterface, error) {
+
 	return mimirtool.New(mimirtool.Config{
-		AuthToken: d.Get("token").(string),
-		User:      d.Get("user").(string),
-		Key:       d.Get("key").(string),
-		Address:   d.Get("url").(string),
+		AuthToken: d.Get("auth_token").(string),
+		User:      d.Get("api_user").(string),
+		Key:       d.Get("api_key").(string),
+		Address:   d.Get("address").(string),
 		ID:        d.Get("tenant_id").(string),
 		TLS: tls.ClientConfig{
-			CAPath:             d.Get("ca_cert_path").(string),
+			CAPath:             d.Get("tls_ca_path").(string),
 			CertPath:           d.Get("tls_cert_path").(string),
 			KeyPath:            d.Get("tls_key_path").(string),
 			InsecureSkipVerify: d.Get("insecure_skip_verify").(bool),
