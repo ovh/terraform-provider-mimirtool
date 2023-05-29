aa5a0ebdfa790d6182967183c703889b44adad99
Wilfried Roset <wilfriedroset@users.noreply.github.com>
2023-05-29 11:23:41 +0200

    Use inline YAML in examples
    Signed-off-by: Wilfried Roset <wilfriedroset@users.noreply.github.com>

--- examples/resources/mimirtool_ruler_namespace/resource.tf
+++ examples/resources/mimirtool_ruler_namespace/resource.tf
@@ -1,4 +1,14 @@
 resource "mimirtool_ruler_namespace" "demo" {
   namespace   = "demo"
-  config_yaml = file("testdata/rules.yaml")
+  config_yaml = <<EOT
+groups:
+- name: mimir_api_1
+  rules:
+  - expr: histogram_quantile(0.99, sum(rate(cortex_request_duration_seconds_bucket[1m]))
+      by (le, cluster, job))
+    record: cluster_job:cortex_request_duration_seconds:99quantile
+  - expr: histogram_quantile(0.50, sum(rate(cortex_request_duration_seconds_bucket[1m]))
+      by (le, cluster, job))
+    record: cluster_job:cortex_request_duration_seconds:50quantile
+EOT
 }
