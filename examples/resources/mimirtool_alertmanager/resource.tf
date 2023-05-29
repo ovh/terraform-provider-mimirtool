5e50687de53e49b57d9084e2588fc6675ebb3c29
Wilfried Roset <wilfriedroset@users.noreply.github.com>
2023-05-29 11:23:41 +0200

    Use inline YAML in examples
    Signed-off-by: Wilfried Roset <wilfriedroset@users.noreply.github.com>

--- examples/resources/mimirtool_alertmanager/resource.tf
+++ examples/resources/mimirtool_alertmanager/resource.tf
@@ -1,6 +1,21 @@
 resource "mimirtool_alertmanager" "demo" {
-  config_yaml = file("testdata/example_alertmanager_config.yaml")
+  config_yaml = <<EOT
+global:
+  smtp_smarthost: 'localhost:25'
+  smtp_from: 'youraddress@example.org'
+templates:
+  - 'default_template'
+route:
+  receiver: example-email
+receivers:
+  - name: example-email
+    email_configs:
+      - to: 'youraddress@example.org'
+EOT
   templates_config_yaml = {
-    default_template = file("testdata/example_alertmanager_template.tmpl")
+    default_template = <<EOT
+{{ define "__alertmanager" }}AlertManager{{ end }}
+{{ define "__alertmanagerURL" }}{{ .ExternalURL }}/#/alerts?receiver={{ .Receiver | urlquery }}{{ end }}
+EOT
   }
 }
